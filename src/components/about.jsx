a4417ca36adb2ac561f1d5c76a0b0f42dc96d76a
Sankalp <sankalp13353@gmail.com>
2025-05-12 22:09:48 +0530

    about

---
+++ src/components/about.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About the Pomodoro Timer</h1>
+      <p>
+        The Pomodoro Technique is a time management method developed by Francesco Cirillo.
+        It uses a timer to break work into intervals, traditionally 25 minutes in length,
+        separated by short breaks. This method helps improve focus and productivity.
+      </p>
+    </div>
+  );
+}
+
+export default About;
