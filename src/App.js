eb531fe522ad81c665ca41d2c621309b7aadb332
Sankalp <sankalp13353@gmail.com>
2025-05-12 22:11:13 +0530

    about jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Footer from './components/footer';
-import Home from './components/Home';
-import Timer from './components/Timer';
-import About from './components/about';  // Import the About component
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
+import Home from '../components/Home';
+import Timer from '../components/Timer';
+import About from '../components/about'; 
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/timer" element={<Timer />} />
-          <Route path="/about" element={<About />} /> {/* Add About route */}
+          <Route path="/about" element={<About />} /> 
         </Routes>
         <Footer />
       </div>
